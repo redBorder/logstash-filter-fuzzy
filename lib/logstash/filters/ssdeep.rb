5e59f246103a754246db3d5d1edc861ead39ae3d
eareyes <eareyes@redborder.com>
2022-08-30 10:56:52 +0100

    Aerospike instead of local db

--- lib/logstash/filters/ssdeep.rb
+++ lib/logstash/filters/ssdeep.rb
@@ -3,32 +3,55 @@ require "logstash/filters/base"
 require "logstash/namespace"
 
 require 'json'
+require 'aerospike'
+require 'text'
+
+require_relative "util/aerospike_config"
 
 class LogStash::Filters::Ssdeep < LogStash::Filters::Base
+
+  include Aerospike
 
   config_name "ssdeep"
 
   # Ssdeep binary path
-  config :ssdeep_bin,      :validate => :string,    :default => "/usr/bin/ssdeep"
-  # Ssdeep database path
-  config :fuzzy_db,        :validate => :string,    :default => "/usr/share/logstash/fuzzy.db"
+  config :ssdeep_bin,                       :validate => :string,           :default => "/usr/bin/ssdeep"
   # Similarity threshold
-  config :threshold,       :validate => :number,    :default => 95
+  config :threshold,                        :validate => :number,           :default => 95
   # File that is going to be analyzed
-  config :file_field,      :validate => :string,    :default => "[path]"
+  config :file_field,                       :validate => :string,           :default => "[path]"
   # Loader weight
-  config :weight,                                   :default => 1.0
+  config :weight,                                                           :default => 1.0
   # Where you want the data to be placed
-  config :target,           :validate => :string,   :default => "ssdeep"
+  config :target,                           :validate => :string,           :default => "ssdeep"
   # Where you want the score to be placed
-  config :score_name,       :validate => :string,   :default => "sb_ssdeep"
+  config :score_name,                       :validate => :string,           :default => "sb_ssdeep"
   # Where you want the latency to be placed
-  config :latency_name,     :validate => :string,   :default => "ssdeep_latency"
+  config :latency_name,                     :validate => :string,           :default => "ssdeep_latency"
+  #Aerospike server in the form "host:port"
+  config :aerospike_server,                 :validate => :string,           :default => ""
+  #Namespace is a Database name in Aerospike
+  config :aerospike_namespace,              :validate => :string,           :default => "malware"
+  #Set in Aerospike is similar to table in a relational database.
+  # Where are fuzzy hashes stored
+  config :aerospike_set_fuzzy_hash,         :validate => :string,           :default => "ssdeep"
+  #Set in Aerospike is similar to table in a relational database.
+  # Where are scores stored
+  config :aerospike_set_scores,             :validate => :string,           :default => "hashScores"
 
 
   public
   def register
     # Add instance variables
+    begin
+      @aerospike_server = AerospikeConfig::servers if @aerospike_server.empty?
+      @aerospike_server = @aerospike_server[0] if @aerospike_server.class.to_s == "Array"
+      host,port = @aerospike_server.split(":")
+      @aerospike = Client.new(Host.new(host, port))
+
+    rescue Aerospike::Exceptions::Aerospike => ex
+      @logger.error(ex.message)
+    end
   end # def register
 
   private
@@ -36,64 +59,114 @@ class LogStash::Filters::Ssdeep < LogStash::Filters::Base
   def get_ssdeep_info
     ssdeep_info = {"Matches" => []}
     score = -1
-    in_database = false
+    matches = []
 
-    unless File.exist?(@ssdeep_bin)
-      @logger.error("Ssdeep binary is not in #{@ssdeep_bin}.")
-      return [ssdeep_info,score]
-    end
-
-    unless File.exist?(@file_path)
-      @logger.error("File #{@file_path} does not exist.")
-      return [ssdeep_info,score]
-    end
-
-    unless File.exist?(@fuzzy_db)
-      @logger.error("Database #{@fuzzy_db} does not exist.")
-      return [ssdeep_info,score]
-    end
-
-    unless check_database
-      @logger.error("Database #{@fuzzy_db} is not well formatted.")
-      return [ssdeep_info,score]
-    end
-
+    #@ssdeep = Ssdeep.from_file(@file_path)
     _,_,_,@ssdeep,_ = `#{@ssdeep_bin} -s #{@file_path}`.split(/[,\n]/)
 
-    results = `#{@ssdeep_bin} -t #{@threshold} -s -m #{@fuzzy_db} #{@file_path}`.split("\n")
+    ssdeep_records = get_ssdeep_records
 
-    results.each do |result|
-      _,hash_and_similarity = result.split(':')
-      db_hash,similarity = hash_and_similarity.split(' ')
-      if db_hash != @hash #Avoid duplicate events
-        similarity = similarity.gsub(/[\(\)]/,'').to_i #Similarity comes in format '(similarity)'
-        score = similarity if similarity > score
-        ssdeep_info["Matches"].push({"hash" => db_hash, "similarity" => similarity})
-      else
-        in_database = true
+    score = 0 if ssdeep_records.records.size == 0
+
+    ssdeep_records.each do |record|
+      key = record.key.user_key
+      if @hash != key  #Avoid duplicate events
+        record.bins.each do |_, ssdeep_hash|
+          similarity = compare_ssdeep(@ssdeep,ssdeep_hash)
+          if similarity > @threshold
+            matches.push(ssdeep_hash)
+            ssdeep_info["Matches"].push({"hash" => key, "ssdeep" => ssdeep_hash ,"similarity" => similarity})
+            score = similarity if similarity > score
+          end
+        end
       end
     end
-    update_database unless in_database
-    score = (score * @weight).round
-    [ssdeep_info, score]
+    score *= @weight
+    [ssdeep_info, score, matches]
   end
 
-  def update_database
-    entry = @ssdeep + ',' + '"' +  @hash + '"'
+  def get_ssdeep_records
+    records = []
     begin
-      open(@fuzzy_db, 'a') do |f|
-        f << entry + "\n"
-      end
-    rescue Errno::ENOENT => ex
+      stmt = Statement.new(@aerospike_namespace, @aerospike_set_fuzzy_hash)
+      records = @aerospike.query(stmt)
+    rescue Aerospike::Exceptions::Aerospike => ex
+      @logger.error(ex.message)
+    end
+    records
+  end
+
+  def compare_ssdeep(fh1,fh2)
+    #fh fuzzy hash
+    block_size1, str11, str12 = fh1.split(':')
+    block_size2, str21, str22 = fh2.split(':')
+    block_size1 = block_size1.to_i
+    block_size2 = block_size2.to_i
+
+    case block_size1
+      when block_size2
+        score1 = string_score(str11, str21, block_size1)
+        score2 = string_score(str12, str22, block_size2)
+        score = [score1, score2].min
+
+      when block_size2 * 2
+        score = string_score(str11, str22, block_size1)
+
+      when block_size2 / 2
+        score = string_score(str12, str21, block_size2)
+      else
+        score = 0
+    end
+
+    score.truncate
+  end
+
+  def string_score(str1,str2,block_size)
+    spamsum_length = 64
+    min_blocksize = 3
+
+    len1 = str1.size
+    len2 = str2.size
+
+    return 0 if (len1 > spamsum_length || len2 > spamsum_length)
+
+    lev_score = Text::Levenshtein.distance(str1, str2).to_f
+    # Normalized Levenshtein Distance
+    lev_score /= [len1,len2].max
+
+    score = (1 - (lev_score / (len1 + len2))) * 100
+
+    score_aux = block_size / min_blocksize * [len1,len2].min
+
+    [score, score_aux].min
+  end
+
+  def update_aerospike(matches,score)
+
+    begin
+      #if !matches.empty? || @file_score > 0   #TODO Check score to know if we should add the fuzzy hash to aerospike
+        # First add ssdeep hash to @aerospike_set_fuzzy_hash
+        key = Key.new(@aerospike_namespace,@aerospike_set_fuzzy_hash,@hash)
+        bin = Bin.new(@target, @ssdeep)
+        @aerospike.put(key,bin,WritePolicy.new)
+      #end
+
+    rescue Aerospike::Exceptions::Aerospike => ex
       @logger.error(ex.message)
     end
   end
 
-  # Check First line in Fuzzy database
-  def check_database
-    File.open(@fuzzy_db, &:readline) == "ssdeep,1.1--blocksize:hash:hash,filename\n"
+  def get_aerospike_value(key,field)
+    value = nil
+    record = @aerospike.get(key,[],Policy.new)
+    record.bins.each do |k,v|
+      if k == field
+        value = v
+        break
+      end
+    end
+    value
   end
-
 
   public
   def filter(event)
@@ -101,13 +174,15 @@ class LogStash::Filters::Ssdeep < LogStash::Filters::Base
     @file_path = event.get(@file_field)
 
     begin
-      @hash = Digest::MD5.hexdigest File.read @file_path
+      @hash = Digest::SHA2.new(256).hexdigest File.read @file_path
     rescue Errno::ENOENT => ex
       @logger.error(ex.message)
     end
 
     starting_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-    ssdeep_result,score = get_ssdeep_info
+
+    ssdeep_result, score, matches = get_ssdeep_info
+    update_aerospike(matches,score)
 
     ending_time  = Process.clock_gettime(Process::CLOCK_MONOTONIC)
     elapsed_time = (ending_time - starting_time).round(1)
